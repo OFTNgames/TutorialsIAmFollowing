0798f083eaef75bee86b0be5584e5b979da86e4d
OFTNgames <pmenesick@gmail.com>
2021-02-01 16:46:51 -0500

    Intial unity project

---
+++ Assets/Scripts/SteamTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamTest : MonoBehaviour
+{
+    void Start()
+    {
+        if (!SteamManager.Initialized) { return; }
+
+        string name = Steamworks.SteamFriends.GetPersonaName();
+        Debug.Log(name);
+    }
+}
